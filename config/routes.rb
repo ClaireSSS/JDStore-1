ec593f987288012403af63e21a5767c4bc70957c
shaona <shaonasn@163.com>
2017-05-18 19:07:45 +0800

    implement add_product_to_cart function

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root 'products#index'
   devise_for :users
   namespace :admin do
-    resources :products do
-      member do
-        post :add_to_cart
-      end
-    end
+  resources :products
   end
 
-  resources :products
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
 end
