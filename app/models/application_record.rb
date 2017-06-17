0f965d044c0a17f60de6a00e2ef38e90fa74db45
shaona <shaonasn@163.com>
2017-06-17 16:56:03 +0800

    add token to order

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,9 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  belongs_to :user
-
-  validates :billing_name, presence: true
-  validates :billing_address, presence: true
-  validates :shipping_name, presence: true
-  validates :shipping_address, presence: true
 end
