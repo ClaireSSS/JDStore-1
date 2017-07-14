297602e2f4c76d54e46c2b4d21debb0a271bb12a
shaona <shaonasn@163.com>
2017-07-14 22:10:39 +0800

    all the orders can be seen

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :products
   end
 
+  namespace :account do
+    resources :orders
+  end
+
   resources :carts
   resources :carts do
     collection do
